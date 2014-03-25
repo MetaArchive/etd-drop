c63222762bbfa2ea6f303ddb28eac0442415aad3
Stephen Eisenhauer <bhs2007@gmail.com>
2014-03-25 15:02:57 -0500

    Initial work on main app

---
+++ etd_drop_app/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, url
+
+from . import views
+
+urlpatterns = patterns('',
+	# / (Home page)
+	url(r'^$', 'etd_drop_app.views.index', name='index'),
+
+	# /login
+    url(r'^login$', 'django.contrib.auth.views.login', {'extra_context': {'title': 'Log In'}}),
+    # /logout
+    url(r'^logout$', 'etd_drop_app.views.logout_view'),
+
+    # /submit
+    url(r'^submit$', 'etd_drop_app.views.submit'),
+)
--- etd_drop_app/views.py
+++ etd_drop_app/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.template import RequestContext
+from django.contrib.auth import logout
+from django.contrib import messages
 
-# Create your views here.
+
+def logout_view(request):
+	'''Logs the user out and redirects home'''
+	logout(request)
+	messages.success(request, "You have been logged out successfully.")
+	return redirect('/')
+
+def index(request):
+	'''Homepage, with login form'''
+	context = RequestContext(request)
+	context['title'] = "Welcome"
+	return render(request, 'etd_drop_app/index.html', context)
+
+def submit(request):
+	'''Submit page, with submission form'''
+	context = RequestContext(request)
+	context['title'] = "Submit an ETD"
+	return render(request, 'etd_drop_app/submit.html', context)
