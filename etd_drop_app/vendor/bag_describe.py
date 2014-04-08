b42660d38d58f6b3bd3f82274e723e5e74e377a3
Stephen Eisenhauer <bhs2007@gmail.com>
2014-04-08 17:28:34 -0500

    Complete DAITSS Description Service integration

--- etd_drop_app/vendor/bag_describe.py
+++ etd_drop_app/vendor/bag_describe.py
@@ -1,14 +1,23 @@
 import os
+import urllib2
+
+import requests  # pip install requests
+
 
 def bag_describe(url, bag_path):
-    '''Describes a bag using an instance of the DAITSS Format Description 
-    Service running at url.
-    
+    '''Describes a bag using an instance of the DAITSS Format Description
+    Service running at url. Creates a 'premis' tag directory in bag's root
+    containing description service outputs per file in bag payload.
+
+    Note: If you have a tagmanifest, you should use a BagIt utility to update
+    it after this operation. Otherwise, these tags will not be accounted for.
+
     url - e.g. "http://localhost:3000"
     bag_path - Path to the top level of a BagIt bag
     '''
     bag_path = os.path.abspath(bag_path)
     data_dir = os.path.join(bag_path, 'data')
+    describe_url = "%s/describe" % url
 
     if not os.path.isdir(bag_path):
         raise Exception("Not a valid path to a directory: %s" % bag_path)
@@ -17,11 +26,25 @@ def bag_describe(url, bag_path):
     if not os.path.isfile(os.path.join(bag_path, 'bagit.txt')):
         raise Exception("Bag path contains no bagit.txt file: %s" % bag_path)
 
-    # TODO: Verify description service is responsive
+    # Verify description service is reachable
+    if urllib2.urlopen(url).getcode() is not 200:
+        raise Exception(
+            "Description service not reachable at given URL: %s" % url
+        )
 
+    # Walk through all files/directories
     os.chdir(data_dir)
     for dirpath, dirnames, filenames in os.walk('.'):
-        os.makedirs(os.path.join(bag_path, 'premis', dirpath))
+        current_dir = os.path.join(bag_path, 'premis', dirpath)
+        os.makedirs(current_dir)
         for filename in filenames:
-            # TODO: Call description service
-            pass
+            extension = os.path.splitext(filename)[1]
+            filepath = os.path.join(dirpath, filename)
+            with open(filepath, 'r') as document:
+                r = requests.post(
+                    describe_url,
+                    files={'document': document},
+                    data={'extension': extension}
+                )
+                with open(os.path.join(current_dir, filename+'.xml'), 'w') as premis:
+                    premis.write(r.text)
